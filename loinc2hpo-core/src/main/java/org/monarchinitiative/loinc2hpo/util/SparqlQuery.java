b73fd03966cdac4c8666d5563b18beb4078053cd
Aaron Zhang <kingmanzhang@gmail.com>
2017-12-18 22:45:48 -0500

    start working on Jena API for sparql

--- loinc2hpo-core/src/main/java/org/monarchinitiative/loinc2hpo/util/SparqlQuery.java
+++ loinc2hpo-core/src/main/java/org/monarchinitiative/loinc2hpo/util/SparqlQuery.java
@@ -24,12 +24,10 @@ public class SparqlQuery {
         this.tissue = tissue;
     }
 
-    public static void main(String[] args) {
-        String hpo = SparqlQuery.class.getResource("hp.owl").getPath();
-        System.out.println("hpo path: " + hpo);
+    public static Model getOntologyModel(String ontologyFile) {
         Model model = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM, null);
         try {
-            InputStream in = FileManager.get().open(hpo);
+            InputStream in = FileManager.get().open(ontologyFile);
             try {
                 model.read(in, null);
                 System.out.println("model created");
@@ -39,17 +37,53 @@ public class SparqlQuery {
         } catch (JenaException je) {
             System.out.println("cannot open hpo.owl");
         }
-        /**
-        String queryText = "SELECT * WHERE {?s ?p ?o} LIMIT 10";
-        Query query = QueryFactory.create(queryText);
-        QueryExecution qexec = QueryExecutionFactory.create(query, model);
-        Iterator<QuerySolution> results = qexec.execSelect();
-        if (results.hasNext()) {
-            System.out.println(results.next().toString());
+        return model;
+    }
+
+    public static ResultSet query(Model model, String queryString) {
+        Query query = QueryFactory.create(queryString);
+        try (QueryExecution qexec = QueryExecutionFactory.create(query, model)) {
+            return qexec.execSelect();
         }
-         **/
     }
 
 
+    public static void main(String[] args) {
+        String hpo = SparqlQuery.class.getResource("/hp.owl").getPath(); //need '/' to get a resource file
+        System.out.println("hpo path: " + hpo);
+        Model model = getOntologyModel(hpo);
+        String prefix = "PREFIX xmlns: <http://purl.obolibrary.org/obo/hp.owl#> "+
+                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
+                "PREFIX owl:<http://www.w3.org/2002/07/owl#> " +
+                "PREFIX oboInOwl: <http://www.geneontology.org/formats/oboInOwl#> " +
+                "PREFIX hsapdv: <http://purl.obolibrary.org/obo/hsapdv#> " +
+                "PREFIX hp: <http://purl.obolibrary.org/obo/hp#> " +
+                "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> " +
+                "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> " +
+                "PREFIX hp2: <http://purl.obolibrary.org/obo/hp.owl#> " +
+                "PREFIX obo: <http://purl.obolibrary.org/obo/> " +
+                "PREFIX dc: <http://purl.org/dc/elements/1.1/> ";
+        String selection = "SELECT DISTINCT ?phenotype ?label " +
+                "WHERE {?phenotype obo:IAO_0000115 ?definition . " +
+                "?phenotype rdfs:label ?label . ";
+
+        String parameter = "Testosterone";
+
+        String filter =  String.format("FILTER (regex(?definition, \"%s\", \"i\") || regex(?label, \"%s\", \"i\")) " +
+                "FILTER (regex(?definition, \"increased|decreased|elevated|reduced|high|low|above|below\", \"i\") || regex(?label, \"increased|decreased|elevated|reduced|high|low|above|below|hyper|hypo\", \"i\"))\n" +
+                "} ", parameter, parameter);
+
+        String queryText =  prefix + selection + filter;
+
+        Query query = QueryFactory.create(queryText);
+        QueryExecution qexec = QueryExecutionFactory.create(query, model);
+        Iterator<QuerySolution> results = qexec.execSelect();
+        int count = 0;
+        while (results.hasNext()) {
+            count++;
+            System.out.println(results.next().toString());
+        }
+System.out.println(count + " results are found!");
+    }
 
 }
