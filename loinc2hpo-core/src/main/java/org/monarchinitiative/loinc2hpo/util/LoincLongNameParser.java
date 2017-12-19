189b5b4baf6d3a83128b3cde27b76a2e86f11a96
Aaron Zhang <kingmanzhang@gmail.com>
2017-12-18 22:45:02 -0500

    class to parse a loinc long common name

--- loinc2hpo-core/src/main/java/org/monarchinitiative/loinc2hpo/util/LoincLongNameParser.java
+++ loinc2hpo-core/src/main/java/org/monarchinitiative/loinc2hpo/util/LoincLongNameParser.java
@@ -18,58 +18,95 @@ public class LoincLongNameParser {
         //split loinc long common name by "by"
         //example: "Erythrocyte distribution width [Ratio] by Automated count"
         //example: "Platelets [#/volume] in Blood by Automated count"
-        String[] elems1 = this.loincLongName.split("by");
+        String[] elems1 = this.loincLongName.split(" by ");
         if (elems1.length == 2) {
             this.assayMethod = elems1[1]; //this is the method for the Loinc assay
         }
-        String[] elems2 = elems1[0].split("in");
+        String[] elems2 = elems1[0].split(" in ");
         if(elems2.length == 1) {//if no "in", then it might be "of". example: Hematocrit [Volume Fraction] of Blood
-            elems2 = elems2[0].split("of");
+            elems2 = elems2[0].split(" of ");
         }
         if(elems2.length == 1) {
-            String[] elems3 = elems2[0].split("\\[");
+            String[] elems3 = elems2[0].split(" \\[");
+            if (elems3.length == 1) {
+                this.parameter = elems3[0];
+            }
             if (elems3.length == 2) {
                 this.parameter = elems3[0];
                 this.assayType = elems3[1].split("\\]")[0];
             }
         }
+        if(elems2.length == 3) { //e.g. "Cholesterol in HDL [Presence] in Serum or Plasma"
+            elems2 = new String[]{elems2[0] + " in " + elems2[1], elems2[2]};
+        }
         if(elems2.length == 2) { //"Platelets [#/volume]", "Blood"
             this.tissue = elems2[1];
-            String[] elems3 = elems2[0].split("\\[");
+            String[] elems3 = elems2[0].split(" \\[");
+            if (elems3.length == 1) {
+                this.parameter = elems3[0];
+            }
             if (elems3.length == 2) {
                 this.parameter = elems3[0];
                 this.assayType = elems3[1].split("\\]")[0];
+                if(elems3[1].split("\\]").length == 2) {
+                    this.parameter = this.parameter + elems3[1].split("\\]")[1];
+                }
             }
         }
 
     }
 
+    /**
+     * Extract and return the parameter from a Loinc long common name.
+     * e.g. "Platelet mean volume [Entitic volume] in Blood by Automated count"
+     * The method will return "Platelet mean volume".
+     * @return parameter measured from a Loinc long common name; "" if such information is not identified.
+     */
     public String getLoincParameter(){
 
-//        if(this.parameter == null) {
-//            throw new ParseException("Loinc Parameter is not found!", 1);
-//        }
         return this.parameter;
 
     }
 
+    /**
+     * Extract and return the tissue from a Loinc long common name.
+     * e.g. "Platelet mean volume [Entitic volume] in Blood by Automated count"
+     * The method will return "Blood".
+     * @return tissued measured from a Loinc long common name; "" if such information is not identified.
+     */
     public String getLoincTissue(){
 
         return this.tissue;
 
     }
 
+    /**
+     * Extract and return the method from a Loinc long common name.
+     * e.g. "Platelet mean volume [Entitic volume] in Blood by Automated count"
+     * The method will return "Automated count".
+     * @return method used from a Loinc long common name; "" if such information is not identified.
+     */
     public String getLoincMethod(){
 
         return this.assayMethod;
     }
 
+    /**
+     * Extract and return the type of a Loinc assay from a Loinc long common name.
+     * e.g. "Platelet mean volume [Entitic volume] in Blood by Automated count"
+     * The method will return "Entitic volume" (Strings in brackets).
+     * @return type used from a Loinc long common name; "" if such information is not identified.
+     */
     public String getLoincType(){
 
         return this.assayType;
 
     }
 
+    /**
+     * Override the toString method to get the complete information.
+     * @return information identified from a Loinc long common name.
+     */
     @Override
     public String toString() {
         StringBuilder builder = new StringBuilder();
@@ -80,20 +117,5 @@ public class LoincLongNameParser {
         builder.append("assay type: " + this.assayType + "\n");
         return builder.toString();
     }
-
-    public static void main(String[] args) {
-        String name1 = "Potassium [Moles/volume] in Serum or Plasma";
-        LoincLongNameParser parser = new LoincLongNameParser(name1);
-        System.out.println(parser);
-
-        String name2 = "Erythrocyte distribution width [Ratio] by Automated count";
-        parser = new LoincLongNameParser(name2);
-        System.out.println(parser);
-
-        String name3 = "Platelet mean volume [Entitic volume] in Blood by Automated count";
-        parser = new LoincLongNameParser(name3);
-        System.out.println(parser);
-    }
-
 
 }
--- loinc2hpo-core/src/test/org/monarchinitiative/loinc2hpo/util/LoincLongNameParserTest.java
+++ loinc2hpo-core/src/test/org/monarchinitiative/loinc2hpo/util/LoincLongNameParserTest.java
@@ -1,5 +1,147 @@
+package org.monarchinitiative.loinc2hpo.util;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class LoincLongNameParserTest {
+
+    @Test
+    public void testString1() {
+        String name = "Potassium [Moles/volume] in Serum or Plasma";
+        LoincLongNameParser parser = new LoincLongNameParser(name);
+        assertEquals("Potassium", parser.getLoincParameter());
+        assertEquals("Serum or Plasma", parser.getLoincTissue());
+        assertEquals("Moles/volume", parser.getLoincType());
+        assertEquals("", parser.getLoincMethod());
+
+
+    }
+
+    @Test
+    public void testString2() {
+        String name = "Erythrocyte distribution width [Ratio] by Automated count";
+        LoincLongNameParser parser = new LoincLongNameParser(name);
+        assertEquals("Erythrocyte distribution width", parser.getLoincParameter());
+        assertEquals("", parser.getLoincTissue());
+        assertEquals("Ratio", parser.getLoincType());
+        assertEquals("Automated count", parser.getLoincMethod());
+    }
+
+    @Test
+    public void testString3() {
+        String name = "Platelet mean volume [Entitic volume] in Blood by Automated count";
+        LoincLongNameParser parser = new LoincLongNameParser(name);
+        assertEquals("Platelet mean volume", parser.getLoincParameter());
+        assertEquals("Blood", parser.getLoincTissue());
+        assertEquals("Entitic volume", parser.getLoincType());
+        assertEquals("Automated count", parser.getLoincMethod());
+    }
+
+    @Test
+    public void testString4() {
+        String name = "CD3 cells/100 cells in Blood";
+        LoincLongNameParser parser = new LoincLongNameParser(name);
+        assertEquals("CD3 cells/100 cells", parser.getLoincParameter());
+        assertEquals("Blood", parser.getLoincTissue());
+        assertEquals("", parser.getLoincType());
+        assertEquals("", parser.getLoincMethod());
+    }
+
+    @Test
+    public void testString5() {
+        String name = "Oxygen [Partial pressure] adjusted to patient's actual temperature in Arterial blood";
+        LoincLongNameParser parser = new LoincLongNameParser(name);
+        assertEquals("Oxygen adjusted to patient's actual temperature", parser.getLoincParameter());
+        assertEquals("Arterial blood", parser.getLoincTissue());
+        assertEquals("Partial pressure", parser.getLoincType());
+        assertEquals("", parser.getLoincMethod());
+    }
+
+    @Test
+    public void testString6() {
+        String name = "Fibrosis score";
+        LoincLongNameParser parser = new LoincLongNameParser(name);
+        assertEquals("Fibrosis score", parser.getLoincParameter());
+        assertEquals("", parser.getLoincTissue());
+        assertEquals("", parser.getLoincType());
+        assertEquals("", parser.getLoincMethod());
+    }
+
+    @Test
+    public void testString7() {
+        String name = "Microalbumin [Mass/time] in 24 hour Urine by Detection limit <= 1.0 mg/L";
+        LoincLongNameParser parser = new LoincLongNameParser(name);
+        assertEquals("Microalbumin", parser.getLoincParameter());
+        assertEquals("24 hour Urine", parser.getLoincTissue());
+        assertEquals("Mass/time", parser.getLoincType());
+        assertEquals("Detection limit <= 1.0 mg/L", parser.getLoincMethod());
+    }
+
+    @Test
+    public void testString8() {
+        String name = "Anion gap 3 in Serum or Plasma";
+        LoincLongNameParser parser = new LoincLongNameParser(name);
+        assertEquals("Anion gap 3", parser.getLoincParameter());
+        assertEquals("Serum or Plasma", parser.getLoincTissue());
+        assertEquals("", parser.getLoincType());
+        assertEquals("", parser.getLoincMethod());
+    }
+
+    @Test
+    public void testString9() {
+        String name = "Thyrotropin [Units/volume] in Serum or Plasma by Detection limit <= 0.05 mIU/L";
+        LoincLongNameParser parser = new LoincLongNameParser(name);
+        assertEquals("Thyrotropin", parser.getLoincParameter());
+        assertEquals("Serum or Plasma", parser.getLoincTissue());
+        assertEquals("Units/volume", parser.getLoincType());
+        assertEquals("Detection limit <= 0.05 mIU/L", parser.getLoincMethod());
+    }
+
+    @Test
+    public void testString10() {
+        String name = "Cholesterol in HDL [Presence] in Serum or Plasma";
+        LoincLongNameParser parser = new LoincLongNameParser(name);
+        assertEquals("Cholesterol in HDL", parser.getLoincParameter());
+        assertEquals("Serum or Plasma", parser.getLoincTissue());
+        assertEquals("Presence", parser.getLoincType());
+        assertEquals("", parser.getLoincMethod());
+    }
+
+    @Test
+    public void testString11() {
+        String name = "Appearance of Body fluid";
+        LoincLongNameParser parser = new LoincLongNameParser(name);
+        assertEquals("Appearance", parser.getLoincParameter());
+        assertEquals("Body fluid", parser.getLoincTissue());
+        assertEquals("", parser.getLoincType());
+        assertEquals("", parser.getLoincMethod());
+    }
+
+    @Test
+    public void testString12() {
+        String name = "Human papilloma virus 16+18+31+33+35+39+45+51+52+56+58+59+68 DNA [Presence] in Cervix by Probe and signal amplification method";
+        LoincLongNameParser parser = new LoincLongNameParser(name);
+        assertEquals("Human papilloma virus 16+18+31+33+35+39+45+51+52+56+58+59+68 DNA", parser.getLoincParameter());
+        assertEquals("Cervix", parser.getLoincTissue());
+        assertEquals("Presence", parser.getLoincType());
+        assertEquals("Probe and signal amplification method", parser.getLoincMethod());
+    }
+
+
+    @Test
+    public void testString13() {
+        String name = "pH of Urine by Test strip";
+        LoincLongNameParser parser = new LoincLongNameParser(name);
+        assertEquals("pH", parser.getLoincParameter());
+        assertEquals("Urine", parser.getLoincTissue());
+        assertEquals("", parser.getLoincType());
+        assertEquals("Test strip", parser.getLoincMethod());
+    }
 
 }
