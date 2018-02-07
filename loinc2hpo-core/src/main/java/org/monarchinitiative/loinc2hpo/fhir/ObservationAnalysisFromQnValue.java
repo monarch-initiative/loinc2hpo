aac47fc718ee04eb42fd762f484e0cb1dba9cb4d
Aaron Zhang <kingmanzhang@gmail.com>
2018-02-07 10:35:24 -0500

    come back to handle reference ranges

--- loinc2hpo-core/src/main/java/org/monarchinitiative/loinc2hpo/fhir/ObservationAnalysisFromQnValue.java
+++ loinc2hpo-core/src/main/java/org/monarchinitiative/loinc2hpo/fhir/ObservationAnalysisFromQnValue.java
@@ -4,14 +4,15 @@ import org.hl7.fhir.dstu3.model.*;
 import org.hl7.fhir.exceptions.FHIRException;
 import org.monarchinitiative.loinc2hpo.codesystems.Code;
 import org.monarchinitiative.loinc2hpo.codesystems.Loinc2HPOCodedValue;
-import org.monarchinitiative.loinc2hpo.exception.AmbiguousReferenceException;
-import org.monarchinitiative.loinc2hpo.exception.ReferenceNotFoundException;
-import org.monarchinitiative.loinc2hpo.exception.UnrecognizedCodeException;
+import org.monarchinitiative.loinc2hpo.exception.*;
 import org.monarchinitiative.loinc2hpo.loinc.HpoTermId4LoincTest;
 import org.monarchinitiative.loinc2hpo.loinc.Loinc2HPOAnnotation;
 import org.monarchinitiative.loinc2hpo.loinc.LoincId;
+import org.monarchinitiative.loinc2hpo.util.AgeCalculator;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.Year;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -22,6 +23,7 @@ public class ObservationAnalysisFromQnValue implements ObservationAnalysis {
     private Reference subject; //we need subject because the applicable reference may dependent on sex, age ... of subject
     private Quantity valueQuantity;
     private List<Observation.ObservationReferenceRangeComponent> references; // a list of references
+    private Patient patient;
 
     private Observation observation;
     private Map<LoincId, Loinc2HPOAnnotation> annotationMap;
@@ -41,24 +43,33 @@ public class ObservationAnalysisFromQnValue implements ObservationAnalysis {
         this.references = observation.getReferenceRange();
     }
 
+    private Patient getPatient() throws AmbiguousSubjectException, SubjectNotFoundException {
+        Patient patient = FhirResourceRetriever.retrievePatientFromServer(subject);
+        return patient;
+    }
+
     /**
      * Get the age of the patient when the observation was made
      * @return
      */
-    private Age getPatientAge(){
-        Date patientBirth = FhirResourceRetriever.retrievePatientFromServer(subject).getBirthDate();
-        Date testDate;
+    private Age getPatientAge() throws AmbiguousSubjectException, SubjectNotFoundException, FHIRException {
+        if (this.patient == null) this.patient = getPatient();
+        Date patientBirth = this.patient.getBirthDate();
+        Date testDate = null;
         if (observation.hasEffective()) {
-            //testDate = observation.getEffective().castToDate(Base.class);
-
+            //is this going to happen? "Effective" should either be effectiveDateTime or effectivePeriod, which are
+            //handled below
         } else if (observation.hasEffectiveDateTimeType()) {
-
+            testDate = observation.getEffectiveDateTimeType().getValue();
         } else if (observation.hasEffectivePeriod()) {
-
+            testDate = observation.getEffectivePeriod().getStart();
         } else {
             return null;
         }
-        return new Age();
+        LocalDate birth = AgeCalculator.toLocalDate(patientBirth);
+        LocalDate current = AgeCalculator.toLocalDate(testDate);
+        int ageInDays = AgeCalculator.calculateAgeInDays(birth, current);
+        return new Age(); //TODO: complete this
     }
 
     /**
@@ -66,13 +77,29 @@ public class ObservationAnalysisFromQnValue implements ObservationAnalysis {
      * @param component
      * @return
      */
+    //TODO: complete this method
     private boolean withinAgeRange(Observation.ObservationReferenceRangeComponent component){
+        /**
         BigDecimal low = component.getAge().getLow().getValue();
         BigDecimal high = component.getAge().getHigh().getValue();
         String unitReference = component.getAge().getLow().getUnit();
 
-        Age patientAge = getPatientAge();
+        try {
+            Age patientAge = getPatientAge();
+        } catch (FHIRException e) {
+            e.printStackTrace();
+        } catch (AmbiguousSubjectException e) {
+            e.printStackTrace();
+        } catch (SubjectNotFoundException e) {
+            e.printStackTrace();
+        }
         //if (low < patientAge < high) return true;
+         **/
+        return true;
+    }
+
+    private boolean sexMatch(Observation.ObservationReferenceRangeComponent component) {
+
         return true;
     }
 
