9989748fd8726923a710105811be664f12f57f03
Peter Robinson <peter.robinson@jax.org>
2021-10-27 16:58:11 -0400

    moving FHIR code to loinc2hpo-fhir

---
+++ loinc2hpo-fhir/src/main/java/org/monarchinitiative/loinc2hpofhir/fhir2hpo/FhirResourceFaker.java
@@ -0,0 +1,50 @@
+package org.monarchinitiative.loinc2hpofhir.fhir2hpo;
+
+import org.hl7.fhir.dstu3.model.Observation;
+import org.hl7.fhir.dstu3.model.Patient;
+import org.monarchinitiative.loinc2hpocore.loinc.LoincId;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * This interface defines classes that generate fake FHIR resources
+ */
+public interface FhirResourceFaker {
+
+    /**
+     * Generate a fake patient object
+     * @return
+     */
+    Patient fakePatient();
+
+    /**
+     * Generate a list of fake patient objects.
+     * @param num size of the list
+     * @return a list of fake patients with specified size
+     */
+    List<Patient> fakePatients(int num);
+
+    /**
+     * Generate a fake observation object
+     * @return
+     */
+    Observation fakeObservation();
+
+    /**
+     * Generate a fake observation object with pre-defined LOINC and subject.
+     * @param loincId
+     * @param patient subject of the observation
+     * @return an observation object
+     */
+    Observation fakeObservation(LoincId loincId, Patient patient);
+
+    /**
+     * For every patient, generate a list of fake observations for each LOINC test.
+     * @param patientList
+     * @param loincList
+     * @return a map of patient: observations
+     */
+    Map<Patient, List<Observation>> fakeObservations(List<Patient> patientList, List<LoincId> loincList);
+
+}
---
+++ loinc2hpo-fhir/src/main/java/org/monarchinitiative/loinc2hpofhir/fhir2hpo/ObservationAnalysisFromQnValue.java
@@ -0,0 +1,69 @@
+package org.monarchinitiative.loinc2hpofhir.fhir2hpo;
+
+import org.hl7.fhir.dstu3.model.*;
+import org.monarchinitiative.loinc2hpocore.Loinc2Hpo;
+import org.monarchinitiative.loinc2hpocore.exception.*;
+import org.monarchinitiative.loinc2hpocore.codesystems.InternalCode;
+import org.monarchinitiative.loinc2hpocore.codesystems.InternalCodeSystem;
+import org.monarchinitiative.loinc2hpocore.annotationmodel.HpoTerm4TestOutcome;
+import org.monarchinitiative.loinc2hpocore.loinc.LoincId;
+
+import java.util.List;
+
+public class ObservationAnalysisFromQnValue implements ObservationAnalysis {
+
+    private Observation observation;
+    private Loinc2Hpo loinc2Hpo;
+
+
+    public ObservationAnalysisFromQnValue(Loinc2Hpo loinc2Hpo, Observation observation){
+        this.loinc2Hpo = loinc2Hpo;
+        this.observation = observation;
+    }
+
+
+    @Override
+    public HpoTerm4TestOutcome getHPOforObservation() {
+
+        LoincId loincId =
+                FhirObservationUtil.getLoincIdOfObservation(this.observation);
+
+        HpoTerm4TestOutcome hpoTerm4TestOutcome = null;
+        //find applicable reference range
+        List<Observation.ObservationReferenceRangeComponent> references =
+                this.observation.getReferenceRange();
+
+        if (references.size() == 0) {
+            throw Loinc2HpoRuntimeException.referenceRangeNotFound();
+        }
+
+        if (references.size() >= 2){
+            throw Loinc2HpoRuntimeException.ambiguousReferenceRange();
+            //An exception: three reference sizes
+            //it can happen when there is actually one range but coded in three ranges
+            //e.g. normal 20-30
+            //in this case, one range ([20, 30]) is sufficient;
+            //however, it is written as three ranges: ( , 20) [20, 30] (30, )
+            //We should handle this case
+        }
+
+        Observation.ObservationReferenceRangeComponent targetReference = references.get(0);
+        double low = targetReference.hasLow()? targetReference.getLow().getValue().doubleValue() : Double.MIN_VALUE;
+        double high = targetReference.hasHigh() ? targetReference.getHigh().getValue().doubleValue() : Double.MAX_VALUE;
+        double observed =
+                this.observation.getValueQuantity().getValue().doubleValue();
+        InternalCode internalCode;
+        if (observed < low) {
+            internalCode = InternalCode.fromCode("L");
+        } else if (observed > high) {
+            internalCode = InternalCode.fromCode("H");
+        } else {
+            internalCode = InternalCode.fromCode("N");
+        }
+
+        hpoTerm4TestOutcome = loinc2Hpo.query(loincId,
+                InternalCodeSystem.getCode(internalCode));
+
+        return hpoTerm4TestOutcome;
+    }
+}
