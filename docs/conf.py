db2a65ac2587a98284e8f6623ac2dad8823b43db
Peter Robinson <peter.robinson@jax.org>
2021-11-07 13:06:59 -0500

    updates

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.mathjax']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -69,7 +69,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
@@ -85,6 +85,7 @@ todo_include_todos = False
 #
 html_theme = "sphinx_rtd_theme"
 
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
@@ -95,6 +96,10 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+# File list in _static
+html_css_files = [
+    'loinc2hpo.css',
+]
 
 
 # -- Options for HTMLHelp output ------------------------------------------
@@ -152,6 +157,8 @@ texinfo_documents = [
      author, 'loinc2hpo', 'One line description of project.',
      'Miscellaneous'),
 ]
+
+
 
 
 
